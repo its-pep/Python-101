a212dd7aefdd15a2de831ef4cf749bbbda8bf1dc
Jose <103228537+its-pep@users.noreply.github.com>
2023-04-29 11:51:46 -0400

    Update Variables_&_datatypes.py

--- Variables_&_datatypes.py
+++ Variables_&_datatypes.py
@@ -1,5 +1,18 @@
+#assign a variable
 name = "Jimothy"
 print(name)
-
+#using an interger
 name_length = 7
 print(name_length)
+
+#use print type to Identify their types 
+print(type(name))
+print(type(name_length))
+#convert int ot str by using brackets 
+name_length = "4"
+print(type(name_length))
+
+#example of a list 
+name_list = ["tim","jim","jimothy"]
+print(type(name_list))
+
