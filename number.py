40ae0b69e501f0a0d96a42f72c41d86dd069c68d
Jose <103228537+its-pep@users.noreply.github.com>
2023-04-30 09:52:16 -0400

    Create number.py
    Numbers in python

---
+++ number.py
@@ -0,0 +1,15 @@
+#ints = whole numbers while float = values which may contain decimals 
+
+t1_int = 1
+t1_float = 1.0
+
+print(t1_int)
+print(t1_float)
+
+print(type(t1_int))
+print(type(t1_float))
+
+t1_complex = 3.14j
+
+print(t1_complex)
+print(type(t1_complex))
