6cfad425c2eb3152541db885d9c3ef92f0920ee3
Jose <103228537+its-pep@users.noreply.github.com>
2023-05-01 21:33:49 -0400

    Update Lists_demo.py

--- Lists_demo.py
+++ Lists_demo.py
@@ -1,1 +1,10 @@
 #List are ordered, changeable and allow for duplicates
+list1 = ["A", "B", "C", "D", "E", "F"]
+print(list1)
+print(type(list1))
+
+list2 = ["A", 1, 2.2, ["B"], [],]
+print(list2)
+
+list1[0] = "J"
+print(list1)
