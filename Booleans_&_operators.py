98e9c069162b47fd8bad45f3e3deb82809483553
Jose <103228537+its-pep@users.noreply.github.com>
2023-05-01 20:53:22 -0400

    Create Booleans_&_operators.py

---
+++ Booleans_&_operators.py
@@ -0,0 +1,31 @@
+#
+valid = True
+not_valid = False
+
+print(valid)
+print(not_valid)
+
+print((10 >9))
+print((10<5))
+
+#true and false can also be referenced as numbers
+print(bool(0))
+print(bool(1))
+
+print(bool(0) == False) 
+print(bool(1) == True)
+
+print(10 + 10)
+print(10 - 10)
+print(10 / 10)
+
+#Floor division to return the largest possible int
+print(10 // 3)
+
+#assign a variable 
+x = 10
+print(x)
+x = x + 1 
+print(x)
+x += 1
+print(x)
