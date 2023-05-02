0f20ada7e0626419bc5ce0064271456d2e949c3c
Jose <103228537+its-pep@users.noreply.github.com>
2023-05-01 21:21:19 -0400

    Update Tuples_demo.py

--- Tuples_demo.py
+++ Tuples_demo.py
@@ -4,3 +4,19 @@
 tuple_fruits = ("pineapple", "banana", "apple")
 print(tuple_fruits)
 print(type(tuple_fruits))
+
+tuple_numbers = (4, 21, 8)
+print(tuple_numbers)
+print(type(tuple_numbers))
+
+tuple_repeat = ('Combine' ,) * 4
+print(tuple_repeat)
+print(type(tuple_repeat))
+
+mixed_tuple = ("A", 1, ("A",1))
+print(mixed_tuple)
+print(type(mixed_tuple))
+
+tuple_combined = tuple_fruits + tuple_numbers
+print(tuple_combined)
+print(type(tuple_combined))
