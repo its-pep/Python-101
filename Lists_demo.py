c55f58493a9ca26272b08e6655b223d88c9c23e0
Jose <103228537+its-pep@users.noreply.github.com>
2023-05-08 11:23:57 -0400

    Update Lists_demo.py

--- Lists_demo.py
+++ Lists_demo.py
@@ -19,3 +19,21 @@ print(list1)
 
 print(list1.count("C"))
 print(list1)
+
+list4 = [0,8,12,72,54,64,21,]
+print(list4)
+
+#using sort will sort list in numerical value
+list4.sort()
+print(list4)
+
+list4.sort(reverse=True)
+print(list4)
+
+list4 = list1
+print(list4)
+print(list1)
+
+list5 = list4.copy()
+print(list4)
+print(list5)
