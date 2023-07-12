719dd79359a52d4b71c46f77fa56fe6f5e20ef01
Jose <103228537+its-pep@users.noreply.github.com>
2023-07-12 17:23:54 -0400

    Conditionals and loops

---
+++ Conditionals.py
@@ -0,0 +1,24 @@
+if True:
+    print("True")
+
+if False:
+    print("False")
+
+if not False:
+    print("Not False")
+
+if 1 < 1:
+    print("1 < 1")
+elif 1 <= 1:
+    print("1 <= 1")
+else:
+    print("else 1")
+
+if 1 < 1:
+    print("1 < 1")
+elif 1 <= 1:
+    print("1 <= 1")
+elif 2 <= 2:
+    print("2 <= 2")
+else:
+    print("else reached")
