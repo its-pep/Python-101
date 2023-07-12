757bc54a052f0217bebf5f14376d3e6dbd6b344b
Jose <103228537+its-pep@users.noreply.github.com>
2023-07-12 18:19:20 -0400

    Create Day1of100day.py
    Day 1 of 100 days of python we go over the print functions, syntax and syntax errors.

---
+++ Day1of100day.py
@@ -0,0 +1,15 @@
+#1st day we will go over the print function
+# --------------- PRINT FUNCTION ---------------
+# The print() function displays output to the console.
+# Syntax: print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+# Examples:
+print("Hello, World!")  # Print a string
+name = "Alice"
+age = 25
+print("Name:", name, "Age:", age)  # Print multiple objects
+print("One", "Two", "Three") 
+print("Hello","pep ")  
+print("World!")
+# Note: Use print() for displaying output or debugging.
+# ----------------------------------------------
+
